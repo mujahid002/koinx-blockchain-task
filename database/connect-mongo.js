9cbf3398c32fc5401e5ba9c1d0c97042991759d9
Mujahid002 <mujahidshaik2002@gmail.com>
2024-04-25 00:25:20 +0530

    mongo connection

---
+++ database/connect-mongo.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.MONGO_CLIENT}.fyfvm7c.mongodb.net/`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function connectMongo() {
+  try {
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    return client;
+  } catch (err) {
+    console.log(err);
+    await client.close();
+    throw err;
+  }
+}
+
+module.exports = connectMongo;
